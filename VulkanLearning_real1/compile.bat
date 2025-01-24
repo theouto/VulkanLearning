25cbd450dc30e261e3632f27ade77b9420954c4b
tomyo <tomyo@GUNGNIR>
2025-01-24 02:30:24 +0100

    Add project files.

---
+++ VulkanLearning_real1/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+pause
---
+++ VulkanLearning_real1/first_app.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "lve_pipeline.hpp"
+#include "lve_window.hpp"
+#include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+#include "lve_model.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace lve
+{
+	class FirstApp
+	{
+	public:
+		static constexpr int WIDTH = 1280;
+		static constexpr int HEIGHT = 720;
+
+
+		FirstApp();
+		~FirstApp();
+
+		FirstApp(const FirstApp&) = delete;
+		FirstApp& operator=(const FirstApp&) = delete;
+
+		void run();
+
+	private:
+		void loadModels();
+		void createPipeLineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void freeCommandBuffers();
+		void drawFrame();
+		void recreateSwapChain();
+		void recordCommandBuffer(int imageIndex);
+
+		LveWindow lveWindow{ WIDTH, HEIGHT, "WINNING" };
+		LveDevice lveDevice{ lveWindow };
+		std::unique_ptr<LveSwapChain> lveSwapChain;
+		std::unique_ptr<LvePipeline> lvePipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
+		std::unique_ptr<LveModel> lveModel;
+	};
+}
