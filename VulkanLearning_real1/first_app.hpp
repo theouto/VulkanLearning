a6145e8c0cc866a3a0ad77e6de0372d9226ee7d4
theoeo <memes.must.die@gmail.com>
2025-02-04 00:17:23 +0100

    yeah

--- VulkanLearning_real1/first_app.hpp
+++ VulkanLearning_real1/first_app.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "lve_pipeline.hpp"
 #include "lve_window.hpp"
 #include "lve_device.hpp"
-#include "lve_swap_chain.hpp"
 #include "lve_model.hpp"
 #include "lve_game_object.hpp"
+#include "lve_renderer.hpp"
 
 #include <memory>
 #include <vector>
@@ -29,21 +28,10 @@ namespace lve
 
 	private:
 		void loadGameObjects();
-		void createPipeLineLayout();
-		void createPipeline();
-		void createCommandBuffers();
-		void freeCommandBuffers();
-		void drawFrame();
-		void recreateSwapChain();
-		void recordCommandBuffer(int imageIndex);
-		void renderGameObjects(VkCommandBuffer commandBuffer);
 
 		LveWindow lveWindow{ WIDTH, HEIGHT, "WINNING" };
 		LveDevice lveDevice{ lveWindow };
-		std::unique_ptr<LveSwapChain> lveSwapChain;
-		std::unique_ptr<LvePipeline> lvePipeline;
-		VkPipelineLayout pipelineLayout;
-		std::vector<VkCommandBuffer> commandBuffers;
+		LveRenderer lveRenderer{ lveWindow, lveDevice };
 		std::vector<LveGameObject> gameObjects;
 	};
 }
